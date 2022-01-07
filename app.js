b89aed87a4462d4ab9a14088b45c55e073dd5003
cliffxing <76663573+cliffxing@users.noreply.github.com>
2022-01-06 21:46:48 -0500

    V5?

--- app.js
+++ app.js
@@ -41,4 +41,25 @@
       evt.currentTarget.className += " active";
     }
   
-   
+
+
+    var sections = document.querySelectorAll('section');
+    var options = {
+    rootMargin: '0px',
+    threshold: 0.25
+    }
+    var callback = (entries) => {
+    entries.forEach((entry) => {
+    var target = entry.target;
+    if (entry.intersectionRatio >= 0.25) {
+    target.classList.add("is-inview");
+    } else {
+     
+    }
+    })
+    }
+    var observer = new IntersectionObserver(callback, options)
+    sections.forEach((section, index) => {
+    observer.observe(section)
+    })
+ 
